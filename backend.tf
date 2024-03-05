0c53f92d2cca9bfbb758018001838fdae9bc6072
apahomovs <alexanderpahomovs@gmail.com>
2024-03-05 15:49:28 -0600

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    # region = "us-east-1"             #where the bucket is
+    region = "us-east-1"             #where the bucket is
     bucket = "backendvedro"          #bucket name    
-    key    = "tf-lesson10-statefile" #how to name state file
+    key    = "tf-handson1-statefile" #how to name state file
   }
 }
